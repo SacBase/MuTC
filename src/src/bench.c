c4f0246959b7475a5e8940bc6008be892616bb97
Carl Joslin <caj@sac-home.org>
2010-01-07 13:21:30 +0000

    Some modules for mutc

--- src/src/bench.c
+++ src/src/bench.c
@@ -1,10 +1,6 @@
 #define SAC_DO_COMPILE_MODULE 1
 #define SAC_MUTC_FUNAP_AS_CREATE  1
-#define SAC_MUTC_THREAD_MALLOC 0
-#define SAC_MUTC_DISABLE_THREAD_MEM 0
-#define SAC_MUTC_BENCH 0
 #define SAC_MUTC_MACROS  1
-
 #define SAC_BACKEND MUTC
 
 #include "sac.h"
@@ -12,12 +8,39 @@
 
 struct bench {                                                   
   int                num; 
+  char *             name;
   struct s_interval *interval;                                            
 };                                                                       
+
+/*
+int len( char *string){
+  int i=0;
+
+  while ( string[i] != '\0'){
+    i++;
+  }
+
+  return( i);
+}
+
+char *cpy( char *string){
+  int i=0;
+  char *res;
+  res = malloc( sizeof(char) * len(string));
+
+  while ( string[i] != '\0'){
+    res[i] = string[i];
+    i++;
+  }
+
+  return( res);
+}
+*/
                                                                          
 void benchStart( struct bench *interval)                                 
 {                                                                        
-  mtperf_start_interval( interval->interval, 0, interval->num, 0);
+  mtperf_start_interval( interval->interval, 0, interval->num, 
+                         (const char *) interval->name);
   return;                                                                
 }                                                                        
                                                                          
@@ -25,7 +48,8 @@ void benchEnd( struct bench* interval)
 {                                                                        
   mtperf_finish_interval( interval->interval, 0);
   return;                                                                
-}                                                                        
+}                         
+                                               
 /* noop*/
 void benchThis( )                                                        
 {                                                                        
@@ -41,18 +65,39 @@ void benchPrint( struct bench* interval)
 void benchGetInterval_i( struct bench** interval, int num)               
 {                                                                        
   (*interval) = (struct bench*)malloc( sizeof( (*interval)));
-  (*interval)->interval = 
-    (struct s_interval *)malloc(sizeof( 1024/*(*interval)->interval*/));
-  (*interval)->num = num;                                                   
+  (*interval)->interval = mtperf_alloc_intervals(1);
+  (*interval)->num = num;
+  (*interval)->name = (char *)malloc(sizeof(char));
+  (*interval)->name[0] = '\0';
+  return;                                                                
+}                                                                        
+
+void benchGetInterval_s( struct bench** interval, char *name) 
+{                                                                        
+  (*interval) = (struct bench*)malloc( sizeof( (*interval)));
+  (*interval)->interval = mtperf_alloc_intervals(1);
+  (*interval)->num = -1;                                                   
+  (*interval)->name = name;
+  return;                                                                
+}                                                                        
+
+void benchGetInterval_si( struct bench** interval, char * name, int num)
+{                                                                        
+  (*interval) = (struct bench*)malloc( sizeof( (*interval)));
+  (*interval)->interval = mtperf_alloc_intervals(1);
+  (*interval)->num = num;                                    
+  (*interval)->name = name;
   return;                                                                
 }                                                                        
                                                                          
 void benchDestroyInterval( struct bench *interval)                        
 {                                                                        
   mtperf_free_intervals(interval->interval);  
+  free(interval->name);
   free(interval);                                                        
   return;                                                                
-}                                                                        
+}                            
+                                            
 void benchCreate( int* a)                                                
 {                                                                        
   *a=1;                                                                  
---
+++ src/src/read.c
@@ -0,0 +1,85 @@
+#define SAC_DO_COMPILE_MODULE 1
+#define SAC_MUTC_MACROS  1
+#define SAC_BACKEND muTC
+
+#include "svp/fibre.h"
+#include "sac.h"
+#include "cmalloc.h"
+#include "svp/abort.h"
+
+static int FibreCount = 0;
+
+int prod( int n, size_t* shape){
+  int i, total=1;
+
+  for (i=0; i<n; i++){
+    total*=(int)shape[n];
+  }
+
+  return( total);
+}
+
+#define UNTYPED( type)                                                  \
+void read_##type( SAC_ND_PARAM_out( res##type##_nt, type)){             \
+  int i;                                                                \
+  size_t *shape;                                                        \
+                                                                        \
+  SAC_ND_DECL__DESC( local##type##_nt, );                               \
+  SAC_ND_DECL__DATA( local##type##_nt, type, );                         \
+  int SAC_ND_A_MIRROR_DIM( local##type##_nt);                           \
+  int SAC_ND_A_MIRROR_SIZE( local##type##_nt);                          \
+                                                                        \
+  SAC_ND_A_MIRROR_DIM( local##type##_nt) = fibre_rank( FibreCount);     \
+  shape = (size_t *)fibre_shape( FibreCount);                           \
+  SAC_ND_A_MIRROR_SIZE( local##type##_nt) =                             \
+    prod( SAC_ND_A_MIRROR_DIM( local##type##_nt), shape);               \
+                                                                        \
+  SAC_ND_ALLOC__DESC( local##type##_nt,                                 \
+                     SAC_ND_A_MIRROR_DIM( local##type##_nt));           \
+  SAC_ND_A_RC( local##type##_nt) = 2;                                   \
+                                                                        \
+  SAC_ND_A_DESC_DIM( local##type##_nt) =                                \
+    SAC_ND_A_MIRROR_DIM( local##type##_nt);                             \
+  SAC_ND_A_DESC_SIZE( local##type##_nt) =                               \
+    SAC_ND_A_MIRROR_SIZE( local##type##_nt);                            \
+                                                                        \
+  for ( i = 0; i < SAC_ND_A_DIM( local##type##_nt); i++){               \
+    SAC_ND_A_DESC_SHAPE( local##type##_nt, i) = (int)shape[i];          \
+  }                                                                     \
+                                                                        \
+  SAC_ND_ALLOC__DATA( local##type##_nt);                                \
+                                                                        \
+  /* copy and change type  */                                           \
+  for ( i=0; i < SAC_ND_A_SIZE( local##type##_nt); i++){                \
+    switch ( fibre_tag( FibreCount)){                                   \
+    case 0:                                                             \
+      SAC_ND_A_FIELD( local##type##_nt)[i] =                            \
+        (type )(((unsigned long*)fibre_data( FibreCount))[i]);          \
+      break;                                                            \
+    case 1:                                                             \
+      SAC_ND_A_FIELD( local##type##_nt)[i] =                            \
+        (type )(((long*)fibre_data( FibreCount))[i]);                   \
+      break;                                                            \
+    case 2:                                                             \
+      SAC_ND_A_FIELD( local##type##_nt)[i] =                            \
+        (type )(((double*)fibre_data( FibreCount))[i]);                 \
+      break;                                                            \
+    default:                                                            \
+      svp_abort();                                                      \
+      break;                                                            \
+    }                                                                   \
+  }                                                                     \
+  FibreCount++;                                                         \
+  SAC_ND_RET_out( res##type##_nt, local##type##_nt);                    \
+}
+
+
+#define resint_nt (res, (AUD, (NHD, (NUQ, (INT, (GLO, (FPA, )))))))
+#define resdouble_nt (res, (AUD, (NHD, (NUQ, (FLO, (GLO, (FPA, )))))))
+
+#define localint_nt (local, (AUD, (NHD, (NUQ, (INT, (GLO, (FPA, )))))))
+#define localdouble_nt (local, (AUD, (NHD, (NUQ, (FLO, (GLO, (FPA, )))))))
+
+UNTYPED( int)
+UNTYPED( double)
+
