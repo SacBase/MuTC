9923107b4bee0e0998190cc0fe0edceb344b86d7
Carl Joslin <caj@sac-home.org>
2010-01-12 20:08:03 +0000

    Support for printResult with 1-10 arguments

--- src/src/bench.c
+++ src/src/bench.c
@@ -1,41 +1,17 @@
 #define SAC_DO_COMPILE_MODULE 1
 #define SAC_MUTC_FUNAP_AS_CREATE  1
 #define SAC_MUTC_MACROS  1
-#define SAC_BACKEND MUTC
+#define SAC_BACKEND muTC
 
+#include "stdarg.h"
+#include "cmalloc.h"
 #include "sac.h"
-
 
 struct bench {                                                   
   int                num; 
   char *             name;
   struct s_interval *interval;                                            
 };                                                                       
-
-/*
-int len( char *string){
-  int i=0;
-
-  while ( string[i] != '\0'){
-    i++;
-  }
-
-  return( i);
-}
-
-char *cpy( char *string){
-  int i=0;
-  char *res;
-  res = malloc( sizeof(char) * len(string));
-
-  while ( string[i] != '\0'){
-    res[i] = string[i];
-    i++;
-  }
-
-  return( res);
-}
-*/
                                                                          
 void benchStart( struct bench *interval)                                 
 {                                                                        
@@ -60,7 +36,105 @@ void benchPrint( struct bench* interval)
 {                                                                        
   mtperf_report_intervals( interval->interval, 1, REPORT_FIBRE);
   return;                                                                
-}                                                                        
+}
+
+void benchmemcpy( void* toarg, void* fromarg, size_t n){
+  int i;
+  char *to=toarg;
+  char *from=fromarg;
+
+  for (i=0; i<n; i++){
+    to[i] = from[i];
+  }
+}
+
+void benchPrintVector( int n, ...)
+{
+  va_list dots;
+  int i=0;
+  struct s_interval intervals[n];
+
+  va_start( dots, n);
+  for (i=0;i<n;i++){
+    intervals[i] =  *(va_arg( dots, struct bench*)->interval);
+  }
+  va_end( dots);
+
+  mtperf_report_intervals( intervals, n, REPORT_FIBRE);
+}
+
+void benchPrint2( struct bench *a,
+                  struct bench *b){
+  benchPrintVector( 2, a, b);
+}
+void benchPrint3( struct bench *a,
+                  struct bench *b,
+                  struct bench *c){
+  benchPrintVector( 3, a, b, c);
+}
+void benchPrint4( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d){
+  benchPrintVector( 4, a, b, c, d);
+}
+void benchPrint5( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d,
+                  struct bench *e){
+  benchPrintVector( 5, a, b, c, d, e);
+}
+void benchPrint6( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d,
+                  struct bench *e,
+                  struct bench *f){
+  benchPrintVector( 6, a, b, c, d, e, f);
+}
+void benchPrint7( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d,
+                  struct bench *e,
+                  struct bench *f,
+                  struct bench *g){
+  benchPrintVector( 7, a, b, c, d, e, f, g);
+}
+void benchPrint8( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d,
+                  struct bench *e,
+                  struct bench *f,
+                  struct bench *g,
+                  struct bench *h){
+  benchPrintVector( 8, a, b, c, d, e, f, g, h);
+}
+void benchPrint9( struct bench *a,
+                  struct bench *b,
+                  struct bench *c,
+                  struct bench *d,
+                  struct bench *e,
+                  struct bench *f,
+                  struct bench *g,
+                  struct bench *h,
+                  struct bench *i){
+  benchPrintVector( 9, a, b, c, d, e, f, g, h, i);
+}
+void benchPrint10( struct bench *a,
+                   struct bench *b,
+                   struct bench *c,
+                   struct bench *d,
+                   struct bench *e,
+                   struct bench *f,
+                   struct bench *g,
+                   struct bench *h,
+                   struct bench *i,
+                   struct bench *j){
+  benchPrintVector( 10, a, b, c, d, e, f, g, h, i, j);
+}
                                                                          
 void benchGetInterval_i( struct bench** interval, int num)               
 {                                                                        
