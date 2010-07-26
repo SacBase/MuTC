02231fee6d8d6284f2a810a13c3d2c626c8c3327
Nilesh Karavadara <nkk@sac-home.org>
2010-07-26 11:48:43 +0000

    Support for string functions "+" and "tos" added to MUTC.

---
+++ src/src/strcat.c
@@ -0,0 +1,23 @@
+/*
+ *  implementation of standard module StringC
+ */
+
+
+#include "StringC.h"
+
+
+/*****************************************************************/
+
+string SACstrcat( string first, string second)
+{
+  string new;
+
+  new = (string) SAC_MALLOC( strlen( first) + strlen( second) + 1);
+
+  strcpy( new, first);
+  strcat( new, second);
+
+  return( new);
+}
+
+/*****************************************************************/
